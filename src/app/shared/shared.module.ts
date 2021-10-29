91a1a415cdcb0dd758aa578297880c15c580cdd4
Rahan Sharma <rahan.sharma97@gmail.com>
2021-10-30 01:19:06 +0530

    ng Hero Icons Installed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,17 @@ import { MenuItemComponent } from './header/menu-item/menu-item.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
 import { HeroComponent } from './hero/hero.component';
+import { annotation, HeroIconsModule } from 'ng-heroicons';
 
 @NgModule({
-  declarations: [HeaderComponent, MenuItemComponent, SidebarComponent, SidebarItemComponent, HeroComponent],
-  imports: [CommonModule],
+  declarations: [
+    HeaderComponent,
+    MenuItemComponent,
+    SidebarComponent,
+    SidebarItemComponent,
+    HeroComponent,
+  ],
+  imports: [CommonModule, HeroIconsModule.withIcons({ annotation })],
   exports: [HeaderComponent, SidebarComponent],
 })
 export class SharedModule {}
