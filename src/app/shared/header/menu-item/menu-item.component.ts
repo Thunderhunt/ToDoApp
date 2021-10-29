e3a30095f96bce112f4a77bf33e0fa9cb6010836
Rahan Sharma <rahan.sharma97@gmail.com>
2021-10-30 00:02:21 +0530

    Basic Header Setup

---
+++ src/app/shared/header/menu-item/menu-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item',
+  templateUrl: './menu-item.component.html',
+  styleUrls: ['./menu-item.component.scss'],
+})
+export class MenuItemComponent implements OnInit {
+  @Input() right: boolean = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuItemComponent } from './header/menu-item/menu-item.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, MenuItemComponent],
   imports: [CommonModule],
   exports: [HeaderComponent],
 })
