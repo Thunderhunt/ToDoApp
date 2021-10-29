aad9e87d82b58e5ffd7934e32a5aa795bfb5bcef
Rahan Sharma <rahan.sharma97@gmail.com>
2021-10-30 01:03:55 +0530

    Basic Sidebar Component Created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MenuItemComponent } from './header/menu-item/menu-item.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MenuItemComponent],
+  declarations: [HeaderComponent, MenuItemComponent, SidebarComponent, SidebarItemComponent, HeroComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, SidebarComponent],
 })
 export class SharedModule {}
