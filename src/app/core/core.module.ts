737a478c571fcb2431bacedfa96720a128805234
Rahan Sharma <rahan.sharma97@gmail.com>
2021-10-30 21:01:21 +0530

    List Setup

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  star,
+  starSolid,
+  checkCircle,
+  checkCircleSolid,
+  HeroIconsModule,
+} from 'ng-heroicons';
 import { DividerComponent } from './components/divider/divider.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
-  declarations: [DividerComponent],
-  imports: [CommonModule],
-  exports: [DividerComponent],
+  declarations: [DividerComponent, ListItemComponent],
+  imports: [
+    CommonModule,
+    HeroIconsModule.withIcons({
+      star,
+      starSolid,
+      checkCircle,
+      checkCircleSolid,
+    }),
+  ],
+  exports: [DividerComponent, ListItemComponent],
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
 import { HeroComponent } from './hero/hero.component';
-import { annotation, HeroIconsModule } from 'ng-heroicons';
 import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [SidebarComponent, SidebarItemComponent, HeroComponent],
-  imports: [
-    CommonModule,
-    HeroIconsModule.withIcons({ annotation }),
-    CoreModule,
-  ],
+  imports: [CommonModule, CoreModule],
   exports: [SidebarComponent],
 })
 export class SharedModule {}
