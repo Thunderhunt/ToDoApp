e662b858dca9fe6860f7f7cda749557af29f63ca
Rahan Sharma <rahan.sharma97@gmail.com>
2021-11-28 14:04:21 +0530

    Basic Color Pallete Customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_light: "#d1fae5",
+        primary_dark: "#111827",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
